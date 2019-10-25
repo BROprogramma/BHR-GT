6dcbd37aa43e95898104b137337236e1943e36f7
Linda van den Brink <l.vandenbrink@geonovum.nl>
2019-10-25 08:32:52 +0200

    add respec files

---
+++ config.js
@@ -0,0 +1,31 @@
+var respecConfig = {
+  specStatus: "GN-WV",
+  specType: "IM",
+  //canonicalURI: "WV",
+  //publishDate: "2019-03-25",
+  maxTocLevel: 3,
+  editors: [
+    {
+      name: "TNO Geologische Dienst Nederland",
+      url: "https://www.tno.nl/nl/",
+    },
+    {
+      name: "Geonovum",
+      url: "https://www.geonovum.nl", 
+    },  
+  ],
+  shortName: "bhr-gt",
+  pubDomain: "bro",
+  github: "https://github.com/BROprogramma/BHR-GT",
+  //previousPublishDate: "2019-03-25",
+  //previousStatus: "GN-CV",
+  //localBiblio: {
+  //  "ISO19136": {
+  //    "href": "https://www.iso.org/standard/32554.html",
+  //    "title": "ISO 19136:2007 Geographic information -- Geography Markup Language (GML)",
+  //    "authors": [""],
+  //    "date": "September 2007",
+  //    "publisher": "International Organization for Standardization",
+  //  },
+  //},
+};
