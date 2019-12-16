ae91db71bed0290737ccbb274ac10be3821c9845
Linda van den Brink <l.vandenbrink@geonovum.nl>
2019-12-16 11:13:04 +0100

    Config terug voor werkversie

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 var respecConfig = {
-  specStatus: "GN-CV",
+  specStatus: "GN-WV",
   specType: "IM",
   //canonicalURI: "WV",
-  publishDate: "2019-10-31",
+  //publishDate: "2019-10-31",
   maxTocLevel: 3,
   editors: [
     {
@@ -17,8 +17,8 @@ var respecConfig = {
   shortName: "bhr-gt",
   pubDomain: "bro",
   github: "https://github.com/BROprogramma/BHR-GT",
-  //previousPublishDate: "2019-03-25",
-  //previousStatus: "GN-CV",
+  previousPublishDate: "2019-10-31",
+  previousStatus: "GN-CV",
   //localBiblio: {
   //  "ISO19136": {
   //    "href": "https://www.iso.org/standard/32554.html",
