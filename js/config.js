377fef2a30548475b1c4d9168b30070baa4c85ba
Jacob Vos <70584938+jacobvosimpronotion@users.noreply.github.com>
2025-09-26 14:26:32 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -30,7 +30,7 @@ let respecConfig = {
   shortName: "bhr-gt",
   //-- publishDate is verplicht -------------------------------------------------------
   //-- NB: in de werkversie uitzetten, want dan pakt Respec de pushdate ---------------
-  publishDate: "2024-06-13", //JV: test
+  //publishDate: "2024-06-13",
   //custom header lines  
   //eventueel is het mogelijk een versienummer mee te geven, maar bij Geonovum werken we gewoonlijk alleen met datum als onderdeel van de permanente URI.
   publishVersion: "0.0.2", //JV: test
