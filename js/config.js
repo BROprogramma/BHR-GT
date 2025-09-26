08c39c796900fbb2e3ab35d7bc141397998e0a78
Jacob Vos <70584938+jacobvosimpronotion@users.noreply.github.com>
2025-09-26 15:06:41 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -27,7 +27,7 @@ let respecConfig = {
   //licence: "cc0",                 // Public Domain Dedication
   licence: "cc-by",                 // Attribution, met bronvermelding
   //-- shortName is verplicht! (komt in de URL: kies logische afkorting)--------------
-  shortName: "bhr-gt",
+  shortName: "BHR-GT",
   //-- publishDate is verplicht -------------------------------------------------------
   //-- NB: in de werkversie uitzetten, want dan pakt Respec de pushdate ---------------
   publishDate: "2025-09-09",
