3d3476128c9871f350de23bb1d17b631bb76bd23
Jacob Vos <jacobvos@impronotion.nl>
2026-03-02 16:54:16 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ let respecConfig = {
   useLogo: true,
   useLabel: true,
   //-- title is verplicht! Neem hier de titel van het document op ----------------------
-  title: "Basisregistratie Ondergrond Catalogus Overheidsbesluit bodemverontreiniging - Geotechnische boormonsterbeschrijving en boormonsteranalyse",
+  title: "Basisregistratie Ondergrond Catalogus - Geotechnische boormonsterbeschrijving en boormonsteranalyse",
   //subtitle: "Versie 1.0",
   //-- specStatus is verplicht! (activeer 1 van de volgende) --------------------------
   specStatus: "wv",                   // Werkversie
