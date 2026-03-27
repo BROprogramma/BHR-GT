a13f6ccc16fc75f500aa310dba8a1e0874977dbb
Jacob Vos <jacobvos@impronotion.nl>
2026-03-27 15:05:44 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -6,9 +6,9 @@ let respecConfig = {
   //subtitle: "Versie 1.0",
   //-- specStatus is verplicht! (activeer 1 van de volgende) --------------------------
   // specStatus: "wv",                   // Werkversie
-  specStatus: "cv",               // Consultatieversie
+  //specStatus: "cv",               // Consultatieversie
   //specStatus: "vv",               // Versie ter vaststelling
-  //specStatus: "def",              // Vastgestelde versie
+  specStatus: "def",              // Vastgestelde versie
   //specStatus: "basis",            // Basis Document
   //-- specType is verplicht bij alle andere dan BASIS ---------------------------------
   //specType: "NO",                 // Norm
