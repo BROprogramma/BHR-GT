88ead5fe33060d77fc0d8f2cc3de26962c607dd9
Jacob Vos <jacobvos@impronotion.nl>
2026-04-29 08:55:58 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -74,10 +74,6 @@ let respecConfig = {
             value: "088 - 8664 999",
             href: "tel:+31888664999",
           },
-          {
-            value: "088 - 8664 999",
-            href: "javascript:void(0)",
-          },
         ],
       },
     ],
