4f10d0b4a6b7f9f5b4f48a4930d26fa2a6597690
Jacob Vos <jacobvos@impronotion.nl>
2026-04-30 09:13:56 +0200

    'github' element weer disabled

--- js/config.js
+++ js/config.js
@@ -77,7 +77,7 @@ let respecConfig = {
       },
     ],
   //neem hier de URL van de github repository op waar het respec document in staat
-  github: "https://github.com/BROprogramma/BHR-GT",
+  //github: "https://github.com/BROprogramma/BHR-GT",
   postProcess: [
     ...(typeof organisationConfig !== "undefined" &&
     Array.isArray(organisationConfig.postProcess)
