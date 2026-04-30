a5aa70bdeacdad8d70d687a6330d7cd51bc5c870
Jacob Vos <jacobvos@impronotion.nl>
2026-04-30 08:55:45 +0200

    Test wijziging van ST naar IM

--- js/config.js
+++ js/config.js
@@ -13,8 +13,8 @@ let respecConfig = {
   //specStatus: "basis",            // Basis Document
   //-- specType is verplicht bij alle andere dan BASIS ---------------------------------
   //specType: "NO",                 // Norm
-  specType: "ST", // Standaard
-  //specType: "IM",                 // Informatie Model
+  //specType: "ST", // Standaard
+  specType: "IM",                 // Informatie Model
   //specType: "PR",                 // Praktijkrichtlijn
   //specType: "HR",                 // HandReiking
   //specType: "WA",                 // Werkafspraak
