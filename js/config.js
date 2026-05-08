0d7441e10cf45bd6912595ffd78f07dc2abf464d
Jacob Vos <jacobvos@impronotion.nl>
2026-05-08 11:21:00 +0200

    Toevoegen 'edDraftURI' aan config.js

--- js/config.js
+++ js/config.js
@@ -77,6 +77,7 @@ let respecConfig = {
       },
     ],
   //neem hier de URL van de github repository op waar het respec document in staat
+  edDraftURI: "https://github.com/BROprogramma/BHR-GT",
   //github: "https://github.com/BROprogramma/BHR-GT",
   postProcess: [
     ...(typeof organisationConfig !== "undefined" &&
